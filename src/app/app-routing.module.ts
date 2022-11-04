e02399ece554fcf9d9d1f7c7ba6a75aa7dce01ff
Vetal0205 <vetal0205merkulov@gmail.com>
2022-11-04 15:27:13 +0100

    [@Vetal0205] pre-html for home page with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home-page/home.component';
 
-
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home-page/home.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { CommentsSectionComponent } from './comments-section/comments-section.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+    MainSectionComponent,
+    AboutSectionComponent,
+    ServicesSectionComponent,
+    CommentsSectionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
