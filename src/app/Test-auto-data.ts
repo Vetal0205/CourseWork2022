44352047f5744046c5f86c70b77e7660ef627f42
Vetal0205 <vetal0205merkulov@gmail.com>
2022-11-05 17:53:44 +0100

    [@Vetal0205] Technique output on test array (Test-auto-data) realisation on technic-page

---
+++ src/app/Test-auto-data.ts
@@ -0,0 +1,22 @@
+import { IAuto } from "./interfaces/i-auto";
+export const Autos: IAuto[] = [
+    {
+        id: 1,
+        name: 'Козак-2м1',
+        image: "/src/assets/Kozak-2m1.png",
+        power:'', tank:'', fuel_consumption:'', speed:''
+
+    },
+    {
+        id: 2,
+        name: 'HMMWV',
+        image: "/src/assets/HMMWV.png",
+        power:'', tank:'', fuel_consumption:'', speed:''
+    },
+    {
+        id: 3,
+        name: 'Дозор-Б',
+        image: "/src/assets/Dozor-B.png",
+        power:'', tank:'', fuel_consumption:'', speed:''
+    },
+];
