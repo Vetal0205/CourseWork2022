c7489778d9f8e416732648289905a9d12663ea29
Vetal0205 <vetal0205merkulov@gmail.com>
2022-11-07 13:01:21 +0100

    [@Vetal0205] Get element by id:  add/delete/update Mission; (json-server)

--- src/app/services/technic.service.ts
+++ src/app/services/technic.service.ts
@@ -1,21 +1,40 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IAuto } from '../interfaces/i-auto';
+import { IMission } from '../interfaces/i-mission';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Injectable({
   providedIn: 'root'
 })
 export class TechnicService {
+  private apiurl:string = "http://localhost:3000/automobiles";
 
-  url:string="http://localhost:3000/automobiles"
 
+  
   constructor(private http:HttpClient) { }
 
   getTechnic():Observable<IAuto[]>{
-    return this.http.get<IAuto[]>(this.url);
+    return this.http.get<IAuto[]>(this.apiurl);
   }
-  // getTechnicById(id:number):Observable<IAuto>{
-  //   return this.http.get<IAuto>(this.url+"/"+id);
-  // }
+  getTechnicById(id:number):Observable<IAuto>{
+    const url = `${this.apiurl}/${id}`;
+    return this.http.get<IAuto>(url);
+  }
+  addMission(mission: IMission): Observable<Task> {
+    return this.http.post<Task>(this.apiurl, mission, httpOptions);
+  }
+  deleteMission(mission: IMission): Observable<Task> {
+    const url = `${this.apiurl}/${mission.id}`
+    return this.http.delete<Task>(url);
+  }
+  updateMisson(mission: IMission): Observable<Task> {
+    const url = `${this.apiurl}/${mission.id}`;
+    return this.http.put<Task>(this.apiurl, mission, httpOptions);
+  }
 }
