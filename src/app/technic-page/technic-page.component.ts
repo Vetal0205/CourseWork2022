7dc40febb2305de19c86fbf198234b1e22fc2899
Serhii <99029543+ageevprunich@users.noreply.github.com>
2022-11-23 21:56:13 +0200

    @ageevprunich oh shit.....

--- src/app/technic-page/technic-page.component.ts
+++ src/app/technic-page/technic-page.component.ts
@@ -1,23 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { TechnicService } from '../services/technic.service';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-
-export interface Automobiles {
-  picture1: string;
-  picture2: string;
-  picture3: string;
-}
-
-const ELEMENT_DATA: Automobiles[] = [
-  {picture1:'/assets/Dozor-B.jpg' , picture2: '/assets/HMMWV.jpg',picture3:'/assets/Kozak-2m1.jpg' },
-  {picture1:'/assets/Kraz-Spartan.jpg' , picture2: '/assets/Kraz-Cougar.jpg', picture3:'/assets/Kraz-Shrek.jpg' },
-  {picture1:'/assets/T-64BV.jpg' , picture2: '/assets/T-72AMT.jpg', picture3:'/assets/PT-91.jpg'},
-  {picture1:'/assets/T80-BV.jpg' , picture2: '/assets/bmp-1.jpg', picture3:'/assets/bmp-2.jpg'},
-  {picture1:'/assets/m80.jpg' , picture2: '/assets/btr-70.jpg', picture3:'/assets/brm-1k.jpg'},
-  {picture1:'/assets/brdm-2.jpg' , picture2: '/assets/Verba.jpg', picture3:'/assets/Grad.jpg'},
-  {picture1:'/assets/HIMARS.jpg' , picture2: '/assets/M270MLRS.jpg', picture3:'/assets/BM-30Smerch.jpg'},
-];
+import { IAuto } from '../interfaces/i-auto';
 
 
 @Component({
@@ -26,20 +9,19 @@ const ELEMENT_DATA: Automobiles[] = [
   styleUrls: ['./technic-page.component.scss']
 })
 export class TechnicPageComponent implements OnInit {
+  allTechnicList: IAuto[] = [];
+  listLength!: number;
   
-  displayedColumns: string[] = ['picture1','picture2','picture3'];
-  dataSource = new MatTableDataSource<Automobiles>(ELEMENT_DATA);
+  @Input() tech!: IAuto;
   
-  @ViewChild(MatPaginator)
-  paginator!: MatPaginator;
-
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    this.service.getTechnic().subscribe((technics) => { this.allTechnicList = technics; });
   }
   
   constructor(private service: TechnicService) { }
 
   ngOnInit(): void {
+    this.service.getTechnic().subscribe((technics) => { this.allTechnicList = technics; this.listLength = this.allTechnicList.length; });
   }
 
 }
