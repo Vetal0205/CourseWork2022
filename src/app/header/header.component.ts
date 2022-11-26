bf7e42cde7411dd2bea050b06acc9221b46dd343
DenisRudiy <denryd2311@gmail.com>
2022-11-26 19:02:37 +0200

    [@DenisRudiy] adaptability for TechnicPage (part1)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 //import animatons from angular-animations
 import {
-  rubberBandAnimation,
-  bounceInOnEnterAnimation,
-  pulseAnimation
+  bounceInOnEnterAnimation
 } from 'angular-animations';
 
 @Component({
@@ -12,49 +10,10 @@ import {
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   animations: [
-    rubberBandAnimation({duration: 800}),
-    bounceInOnEnterAnimation({ anchor: 'enter', delay: 100 }),
-    pulseAnimation({ anchor: "click", duration: 800, scale: 1.2})
+    bounceInOnEnterAnimation({ anchor: 'enter', delay: 100 })
   ]
 })
 export class HeaderComponent implements OnInit {
-
-  //declare var of animated object
-  header_icon_1 = false;
-  header_icon_2 = false;
-  header_icon_3 = false;
-
-  header_icon_1_click = false;
-  header_icon_2_click = false;
-  header_icon_3_click = false;
-
-  //functions of declared objects
-  mouseEnter_icon_1(){
-    this.header_icon_1 = false;
-    setTimeout(() => { this.header_icon_1 = true; }, 1);
-  }
-  mouseEnter_icon_2(){
-    this.header_icon_2 = false;
-    setTimeout(() => { this.header_icon_2 = true; }, 1);
-  }
-  mouseEnter_icon_3(){
-    this.header_icon_3 = false;
-    setTimeout(() => { this.header_icon_3 = true; }, 1);
-  }
-
-  mouseClick_icon_1(){
-    this.header_icon_1_click = false;
-    setTimeout(() => { this.header_icon_1_click = true; }, 1);
-  }
-  mouseClick_icon_2(){
-    this.header_icon_2_click = false;
-    setTimeout(() => { this.header_icon_2_click = true; }, 1);
-  }
-  mouseClick_icon_3(){
-    this.header_icon_3_click = false;
-    setTimeout(() => { this.header_icon_3_click = true; }, 1);
-  }
-
 
   constructor() { }
 
